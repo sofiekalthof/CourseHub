5aea6431d39c07f23e2a9aa77ef0fe981177427c
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2023-06-17 10:30:13 +0200

    adjust landing page component to use routing from app

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
+import WelcomeMessage from "./WelcomeMessage";
 import Button from "@mui/material/Button";
+import { useContext } from "react";
+import { CompToLoad } from "../App";
+
+// function to return component to load
+function renderSwitch(str) {
+  switch (str) {
+    case "WelcomeMessage":
+      return <WelcomeMessage></WelcomeMessage>;
+    case "Register":
+      return <Register></Register>;
+    case "LogIn":
+      return <Login></Login>;
+    default:
+      return <WelcomeMessage></WelcomeMessage>;
+  }
+}
 
 export default function LandingPage() {
-  const [toDisplay, setToDisplay] = useState("Register");
+  const compProvider = useContext(CompToLoad);
 
   return (
     <>
-      <Button
+      {/* <Button
         onClick={() => setToDisplay("Register")}
         fullWidth
         variant="contained"
@@ -23,7 +40,9 @@ export default function LandingPage() {
         Log-In
       </Button>
 
-      {toDisplay === "Register" ? <Register /> : <Login />}
+      {toDisplay === "Register" ? <Register /> : <Login />} */}
+      {/* <WelcomeMessage></WelcomeMessage> */}
+      {renderSwitch(compProvider.comp)}
     </>
   );
 }
