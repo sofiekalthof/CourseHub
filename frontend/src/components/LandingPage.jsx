c9d9c643b8a4e27697395598e20f0992c4ee2e4e
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2023-06-17 12:49:28 +0200

    removed unnecessary comments

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
 import WelcomeMessage from "./WelcomeMessage";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import { useContext } from "react";
 import { CompToLoad } from "../App";
+import { Typography } from "@mui/material";
 
 // function to return component to load
 function renderSwitch(str) {
@@ -23,26 +26,5 @@ function renderSwitch(str) {
 export default function LandingPage() {
   const compProvider = useContext(CompToLoad);
 
-  return (
-    <>
-      {/* <Button
-        onClick={() => setToDisplay("Register")}
-        fullWidth
-        variant="contained"
-      >
-        Register
-      </Button>
-      <Button
-        onClick={() => setToDisplay("Sign-In")}
-        fullWidth
-        variant="contained"
-      >
-        Log-In
-      </Button>
-
-      {toDisplay === "Register" ? <Register /> : <Login />} */}
-      {/* <WelcomeMessage></WelcomeMessage> */}
-      {renderSwitch(compProvider.comp)}
-    </>
-  );
+  return <>{renderSwitch(compProvider.comp)}</>;
 }
