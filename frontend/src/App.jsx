1e9cc6bfa19fbda54431db2afe8402e64e52a075
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2023-06-17 10:29:24 +0200

    added correct colors to the theme create context-based routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,28 +4,62 @@ import ReactDOM from "react-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { useState } from "react";
+import { createContext } from "react";
 
 const themeDarkGreen = createTheme({
   palette: {
     background: {
-      default: "#42f590",
+      default: "#5CDB95",
       // default: "#ffffff",
     },
     text: {
-      primary: "#303f9f",
+      primary: "#05386B",
+    },
+    primary: {
+      main: "#5CDB95",
+    },
+    secondary: {
+      main: "#05386B",
     },
   },
 });
 
-function App() {
+export const CompToLoad = createContext("LandingPage");
+
+// function to return component to load
+function renderSwitch(str) {
+  switch (str) {
+    case "LandingPage":
+      return <LandingPage></LandingPage>;
+    default:
+      return <LandingPage></LandingPage>;
+  }
+}
+
+export default function App() {
+  const [comp, setComp] = useState("LandingPage");
+  const toRegister = () => {
+    setComp("Register");
+  };
+  const toLogIn = () => {
+    setComp("LogIn");
+  };
+  const toWelcomeMessage = () => {
+    setComp("WelcomeMessage");
+  };
+  const toLandingPage = () => {
+    setComp("LandingPage");
+  };
   return (
     <>
       <ThemeProvider theme={themeDarkGreen}>
         <CssBaseline />
+        <CompToLoad.Provider
+          value={{ comp, toWelcomeMessage, toRegister, toLogIn, toLandingPage }}
+        >
+          {renderSwitch(CompToLoad._currentValue)}
+        </CompToLoad.Provider>
       </ThemeProvider>
-      <LandingPage />
     </>
   );
 }
-
-export default App;
