afd01794eb08283324330e86e68b859105e06498
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2023-06-17 10:31:14 +0200

    removed debug statements

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,6 +6,7 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
+// TODO: move url to .env
 const API_URL = "http://localhost:3600";
 
 export default function Login() {
@@ -18,25 +19,27 @@ export default function Login() {
     event.preventDefault();
 
     try {
-      console.log(`${API_URL}/${email}`);
       // send get request to REST API
       let res = await fetch(`${API_URL}/${email}`, {
         method: "GET",
+        // header neccessary for correct sending of information
         headers: {
           "Content-type": "application/json; charset=UTF-8",
         },
       });
+      // parse return statement
       let resJson = await res.json();
 
-      // if response is successful, reset states
       if (res.status === 200) {
+        // if response is successful, reset states
         setEmail("");
         setPassword("");
-        // debug
+        // some debug commands
         console.log("Form done.");
         alert("User is logged in.");
         console.log(resJson);
       } else {
+        // some debug commands
         console.log("Form returned error from backend.");
         console.log(resJson);
       }
