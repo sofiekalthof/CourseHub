a2410ffb4fb8e968c88820b62f008bbcbbacbb0d
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2023-06-19 12:56:10 +0200

    fixed navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
-import { AppBar, Box, Grid, IconButton, Paper, Toolbar, Typography, Button, Avatar} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import courseHubLogo from '../assets/CourseHubLogo.png';
+import React from "react";
+import {
+  AppBar,
+  Box,
+  Grid,
+  IconButton,
+  Paper,
+  Toolbar,
+  Typography,
+  Button,
+  Avatar,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import courseHubLogo from "../assets/CourseHubLogo.png";
 import { useNavigate } from "react-router-dom";
 
+export default function Navbar() {
+  const navigate = useNavigate();
+  const handleOnClick = () => {
+    navigate("/home");
+  };
 
-export default function Navbar(){
-    const navigate = useNavigate();
-    const handleOnClick =() => {
-        navigate('/')
-    }
-
-    return(
-        <>
-            <Box sx={{flexGrow:1}}>
-                <AppBar position='inherit' color='inherit'>
-                    <Toolbar>
-                        <Box component="img" src={courseHubLogo} sx={{ height: 60 }} onClick={handleOnClick}/>  
-                        <Box sx={{ flexGrow: 1 }} />
-                        <Avatar/>
-
-                    </Toolbar>
-                
-                </AppBar>
-                
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="inherit" color="inherit">
+          <Toolbar>
+            <Box
+              component="img"
+              src={courseHubLogo}
+              sx={{ height: 60 }}
+              onClick={handleOnClick}
+            />
+            <Box sx={{ flexGrow: 1 }} />
+            <Avatar />
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
 }
