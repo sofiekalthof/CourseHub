ef7e3ab98388a312dec3ecfa16284fc03f15006f
sofiekalthof <sofie.kalthof@stud.uni-due.de>
2023-06-19 11:21:03 +0200

    Added Create new button to Homepgae

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,20 +1,27 @@
 import Courses from './Courses';
 import Navbar from './Navbar';
 import SearchBar from './SearchBar';
-import { Card, CardContent, Grid, Typography, Paper, Box } from "@mui/material";
+import { Card, CardContent, Grid, Typography, Paper, Box, Button } from "@mui/material";
 
 function HomePage(){
 
     return(
         <>
         <Grid container spacing={2} sx={{justifyContent:'center'}}>
-            <Grid container item xs={12}> 
+            <Grid item xs={12}> 
                 <Navbar/>
             </Grid>
             <Grid item xs={10}>
-                <Typography>
-                    My Courses
-                </Typography>
+                <Grid container>
+                    <Grid item xs={10.5}>
+                        <Typography>
+                            My Courses
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={1.5}>
+                        <Button variant='outlined'>Create New</Button>
+                    </Grid>
+                </Grid>
             </Grid>
             <Grid item xs={10}> 
                 <SearchBar/>
