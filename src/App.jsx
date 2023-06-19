4d03330b5e7dd2d77a8f3db0ff67fe97b7fda883
sofiekalthof <sofie.kalthof@stud.uni-due.de>
2023-06-19 12:22:26 +0200

    Changed Home Page route

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<HomePage/>}/>
+      <Route path='/home' element={<HomePage/>}/>
       <Route path='/course' element={<CoursePage/>}/>
     </Routes>
 
