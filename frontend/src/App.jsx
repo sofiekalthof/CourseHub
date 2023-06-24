5dcb0ef0fe7a77bd3b838e8f69e484b5d5468ba0
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2023-06-24 17:55:03 +0200

    added conditional rendering based on cookies using global context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import LandingPage from "./components/LandingPage";
-import Login from "./components/Login";
-import Register from "./components/Register";
 import RegisterLogInPage from "./components/RegisterLogInPage";
 import HomePage from "./components/HomePage";
 import CoursePage from "./components/CoursePage";
-import React from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
+// TODO: move url to .env
+const API_URL = "http://localhost:3600";
 
 // new MUI theme with the mock-up website colors
 const themeDarkGreen = createTheme({
@@ -34,20 +34,67 @@ const themeDarkGreen = createTheme({
   },
 });
 
+// create global context to share user cookie with all child components
+export const UserContext = createContext({});
+
 export default function App() {
+  const [userSession, setUserSession] = useState(true);
+
+  const fetchUserAuth = async () => {
+    try {
+      // check if user is authenticated
+      const res = await fetch(`${API_URL}/isAuth`);
+
+      // parse return statement from backend
+      let resJson = await res.json();
+
+      // 401 -> no session found, hence a redirect
+      if (res.status === 401) {
+        // debug messages
+        console.log(`${resJson.msg}. Redirecting to HomePage`);
+        alert(`${resJson.msg}. Redirecting to HomePage`);
+        return;
+      }
+      // set state with user/cookie data
+      setUserSession(resJson);
+    } catch (error) {
+      console.error("There was an error fetch auth", error);
+      return;
+    }
+  };
+
+  // at first render call User Auth
+  useEffect(() => {
+    fetchUserAuth();
+  }, []);
+
   return (
     <>
-      {/* using the theme defined above */}
-      <ThemeProvider theme={themeDarkGreen}>
-        <CssBaseline />
-        <Routes>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/course/:id" element={<CoursePage />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/registerlogin" element={<RegisterLogInPage />} />
-          {/* <Route path="/welcome" element={<Welcome />} /> */}
-        </Routes>
-      </ThemeProvider>
+      {/* using user context */}
+      <UserContext.Provider value={userSession}>
+        {/* using the theme defined above */}
+        <ThemeProvider theme={themeDarkGreen}>
+          <CssBaseline />
+          <Routes>
+            {/* default route is landing page */}
+            <Route path="*" element={<LandingPage />} />
+            {/* if there is a user session, these routes are available */}
+            {userSession._id && (
+              <>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/course/:id" element={<CoursePage />} />
+              </>
+            )}
+            {/* if there is no user session, these routes are available */}
+            {!userSession._id && (
+              <>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/registerlogin" element={<RegisterLogInPage />} />
+              </>
+            )}
+          </Routes>
+        </ThemeProvider>
+      </UserContext.Provider>
     </>
   );
 }
