3fe9b6bff6e816636b45bdf62b5776fbd6505385
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2023-06-24 16:28:59 +0200

    added sessions to the backend login route adjusted accordingly

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require('bcrypt');
-require('dotenv').config();
+const session = require('express-session')
+const MongoDBstore = require('connect-mongodb-session')(session);
 const UserModel = require("./models/dbUsers.js");
 const CourseModel = require("./models/dbCourses.js");
 const TaskModel = require("./models/dbTasks.js");
@@ -18,6 +20,27 @@ app.use(cors());
 
 // Enable parsing of JSON bodies
 app.use(express.json());
+
+// allow connect-mongodb-session library to save sessions under mySessions collection
+const mongoDBstore = new MongoDBstore({
+  uri: process.env.DATABASE_CONNECTION_STRING,
+  collection: 'mySessions',
+});
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    name: 'session-id', // name of the cookies (key field)
+    store: mongoDBstore,
+    cookie: {
+      maxAge: process.env.SESSION_MAX_LENGTH, 
+      sameSite: false,  // same-site and cross-site(diff. schemes, domain or sub-domain) requests
+      secure: process.env.PROD || false, // need an HTTPS enabled browser
+    },
+    resave: true,  // force session to be saved in session store, even if session was not modified
+    saveUninitialized: false,  // dont save session if it was not modified (i.e. no login yet)
+  })
+);
 
 // start listening to the port
 app.listen(port, () => {
@@ -50,7 +73,14 @@ app.route("/login").post(async (req, res) => {
       // check if passwords match
       const matchPassword = await bcrypt.compare(req.body.password, user.password);
       if (matchPassword) {
-        return res.status(200).json({ msg: 'You have logged in successfully' });
+        // create a new session for the user (email and id used to identify user)
+        const userSession = { _id: user._id, email: user.email };
+
+        // attach new session to express-session
+        req.session.user = userSession;
+
+        // status, message and new session (as a cookie) sent to frontend
+        return res.status(200).json({ msg: 'You have logged in successfully', userSession });
       } else {
         return res.status(400).json({ msg: 'Invalid credential' });
       }
