f33c37fb7869f840541e03fc8779c7a292cb148f
sofiekalthof <sofie.kalthof@stud.uni-due.de>
2023-07-03 08:34:34 +0200

    Changed design of Landing Page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,25 +1,17 @@
 import Welcome from "./Welcome";
-import LandingPageHalf from "../assets/LandingPageHalf.png";
+import LandingPageDesign from "../assets/LandingPage.png";
 import { Button, Grid, Typography, Box } from "@mui/material";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
-  const slogan =
-    "Bring Structure to Your Learning,\nBring Success to Your Studies with CourseHub";
   const navigate = useNavigate();
   const handleRegister = () => {
     navigate("/registerlogin");
   };
   return (
     <>
-      <Welcome></Welcome>
-
       <Grid container direction="column">
-        {/*<Grid item xs={12}>
-          <Navbar></Navbar>
-        </Grid>
-  */}
         <Grid item xs={12}>
           <Grid container direction="row" justifyContent="flex-end">
             <Grid item xs={5.25}>
@@ -74,7 +66,7 @@ export default function LandingPage() {
               </Grid>
             </Grid>
             <Grid item xs={6.75}>
-              <img src={LandingPageHalf} width={800} height={700}></img>
+              <img src={LandingPageDesign} width={800} height={700}></img>
             </Grid>
           </Grid>
         </Grid>
