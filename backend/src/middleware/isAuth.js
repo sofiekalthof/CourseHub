96ac8a540fa0b1d96482e8181b753e0f9bd8ac58
UtkuKaradeniz <67807636+UtkuKaradeniz@users.noreply.github.com>
2023-08-07 07:02:49 +0200

    show log

--- backend/src/middleware/isAuth.js
+++ backend/src/middleware/isAuth.js
@@ -1,10 +1,10 @@
 // Route to check auth and return user info in cookie
 exports.isAuth = (req, res) => {
-  // console.log(
-  //   "isAuth Called, userSession(req.session.user) in session-store: ",
-  //   req.session.user
-  // );
-  // console.log("isAuth Called, req.session in session-store: ", req.session);
+  console.log(
+    "isAuth Called, userSession(req.session.user) in session-store: ",
+    req.session.user
+  );
+  console.log("isAuth Called, req.session in session-store: ", req.session);
   if (req.session.user) {
     return res.status(200).json(req.session.user);
   } else {
